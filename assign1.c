86e5153794dc0154c5461d3be50fe9e3e4513b13
Ayush Kumar <ayushkj043@gmail.com>
2024-10-20 17:25:34 +0530

    Add files via upload

---
+++ assign1.c
@@ -0,0 +1,14 @@
+//Program to check if a number is armstrong or not
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter the number:");
+    scanf("%d",&number);
+    int x;
+    x=number%10;
+    printf("%d",x);
+}
+
