8b002108a8caf755e6f58a4307359c60defe30d8
Ayush Kumar <ayushkj043@gmail.com>
2024-11-19 20:12:46 +0530

    push-19

--- Assign-6/assign6-ttt.c
+++ Assign-6/assign6-ttt.c
@@ -25,9 +25,9 @@ int main()
 
     printf("\n\n\t\t\t\t\t\t\tWelcome to the game of Tic-Tac-Toe! \n\nThe working of this game has the following components:\n1. Objective: To be the first to make a straight line with either 'X' or 'O'.\n2. Game Board: The board consists of a 3x3 matrix-like structure, having 9 small boxes.\n3. You win by making your symbol in a row or diagonal or column. Also, as a prt of strategy you need to block your opponent from forming a straight line while making of your own.\n\n\t\t\t\t\t\t   Now enough talk let's start the game. \n\n");
     selection:
-    printf("Choose a mode in which you wanna play the game, Single player(single) or Two player(double?)");
+    printf("\nChoose a mode in which you wanna play the game, Single player(single) or Two player(double?)");
     char mode[10];
-    printf("\nEnter your choice:");
+    printf("\nEnter your choice : ");
     fgets(mode,sizeof(mode),stdin);
     strtok(mode,"\n");
     lowa(mode);
@@ -42,7 +42,7 @@ int main()
     }
     else
     {
-        printf("\n\nError! Enter a valid choice!");
+        printf("\n\nError! Enter a valid choice!\n");
         goto selection;
     }
 
